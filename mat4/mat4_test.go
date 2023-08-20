a4633e468985f8239d041547609bfd611f62c22c
neurlang <77860779+neurlang@users.noreply.github.com>
2023-08-20 09:15:09 +0100

    Update mat4_test.go
    Redirect the imports

--- mat4/mat4_test.go
+++ mat4/mat4_test.go
@@ -4,8 +4,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/rkusa/gm/math32"
-	"github.com/rkusa/gm/vec3"
+	"github.com/neurlang/gm/math32"
+	"github.com/neurlang/gm/vec3"
 )
 
 func TestNew(t *testing.T) {
