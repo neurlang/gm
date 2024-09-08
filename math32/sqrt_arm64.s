67ac3148e7e60390a4995534b94dc14c33cd8078
neurlang <77860779+neurlang@users.noreply.github.com>
2024-09-08 19:22:19 +0200

    Update sqrt_arm64.s

--- math32/sqrt_arm64.s
+++ math32/sqrt_arm64.s
@@ -1,21 +1,15 @@
 #include "textflag.h"
 
 // func Sqrt(x float32) float32
-TEXT ·Sqrt(SB), NOSPLIT, 4-4
+TEXT ·Sqrt(SB), NOSPLIT, $8
     // Load argument (float32 x) from stack into S0
-    MOVW x+0(FP), R0
-    FMOVS R0, S0  // Move from general-purpose register R0 to floating-point register S0
+    LDR S0, [FP, 8]   // Load the float32 argument from the stack frame into S0
 
     // Perform the square root operation using the raw FSQRT instruction
-    WORD $0x1E61C000 // FSQRT S0, S0 in ARM64 assembly
+    FSQRT S0, S0      // Compute the square root of S0
 
     // Store the result (float32) back on the stack
-    FMOVS S0, R0   // Move result back to general-purpose register R0
-    MOVW R0, ret+0(FP)  // Store the result from R0 back to the return value
+    STR S0, [FP, 8]   // Store the result from S0 back to the stack frame
 
     // Return
     RET
-
-// Declare argument and return symbol offsets
-GLOBL x+0(SB), 4, $0
-GLOBL ret+0(SB), 4, $0
